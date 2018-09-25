9d4b275093baf939890d5e159597431ddf4fa08a
Leonille Christie <leonillechristie@gmail.com>
2018-09-25 17:54:36 +0800

    Updated Create function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -44,39 +44,39 @@ class UserController extends Controller
   {
     $results = ['statusCode' => 200];
     try {
-        $formData = $request->all();
-        $requiredFields = ['name', 'email', 'avatar'];
+      $formData = $request->all();
+      $requiredFields = ['name', 'email', 'avatar'];
 
-        foreach ($requiredFields as $field) {
-          if (!array_key_exists($field, $formData)) {
-            throw new \InvalidArgumentException("Missing required field: {$field}");
-          }
-
-          if (strlen($formData[$field]) < 1) {
-            throw new \InvalidArgumentException("Invalid field: {$field}, must be a valid string.");   
-          }
-        }
-        $user = new User;
-
-        $user->name = $request->input('name');
-        $user->email = $request->input('email');
-        $user->avatar = $request->avatar;
-        $user->password = Hash::make('secret');
-
-        $existingUser = User::where('email', '=', $formData['email'])->first();
-
-        if (is_a($existingUser, 'App\User')) {
-          throw new \RuntimeException('Email is already used by another user.');
+      foreach ($requiredFields as $field) {
+        if (!array_key_exists($field, $formData)) {
+          throw new \InvalidArgumentException("Missing required field: {$field}");
         }
 
-        if (!filter_var($formData['email'], FILTER_VALIDATE_EMAIL)) {
-          throw new \RuntimeException('Not a valid email address.');
+        if (strlen($formData[$field]) < 1) {
+          throw new \InvalidArgumentException("Invalid field: {$field}, must be a valid string.");   
         }
+      }
+      $user = new User;
 
-        $user->save();
+      $user->name = $request->input('name');
+      $user->email = $request->input('email');
+      $user->avatar = $request->avatar;
+      $user->password = Hash::make('secret');
+
+      $existingUser = User::where('email', '=', $formData['email'])->first();
+
+      if (is_a($existingUser, 'App\User')) {
+        throw new \RuntimeException('Email is already used by another user.');
+      }
+
+      if (!filter_var($formData['email'], FILTER_VALIDATE_EMAIL)) {
+        throw new \RuntimeException('Not a valid email address.');
+      }
+
+      $user->save();
     } catch(\Exception $e) {
-        $results['error'] = $e->getMessage();
-        $results['statusCode'] = 500;
+      $results['error'] = $e->getMessage();
+      $results['statusCode'] = 500;
     }
 
     return response()->json($results, $results['statusCode']);
