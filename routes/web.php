7afe8122ff2bbf13be0c0825f8fbf386f0ef4065
Leonille Christie <leonillechristie@gmail.com>
2018-09-28 17:16:50 +0800

    updated routes php

--- routes/web.php
+++ routes/web.php
@@ -30,19 +30,19 @@ $router->group(['prefix'=>'api/v1'], function() use($router){
 	$router->get('/users/{id}/products', 'UserController@getAllProducts');		
 	$router->post('/orders', 'OrderController@addOrder');
 	$router->delete('/orders/{id}', 'OrderController@removeOrder');
-	// $router->delete('/orders/{id}/products/{productId}', function($id, $productId) {
-	// 	$order = App\Order::with('products')->find($id);
-	// 	$order->products()->detach($productId);
+	$router->delete('/orders/{id}/products/{productId}', function($id, $productId) {
+		$order = App\Order::with('products')->find($id);
+		$order->products()->detach($productId);
 
-	// 	$order->products()->get()->each(function ($product) use($productId) {
-	// 		if ($product->id == $productId) {
-	// 			$product->delete();
-	// 		}
-	// 	});
-	// 	$order = App\Order::with('products')->find($id);
+		$order->products()->get()->each(function ($product) use($productId) {
+			if ($product->id == $productId) {
+				$product->delete();
+			}
+		});
+		$order = App\Order::with('products')->find($id);
 
-	// 	return response()->json($order);
-	// });
+		return response()->json($order);
+	});
 });
 
 $router->group(['middleware' => 'auth', 'prefix'=>'api/v1'], function () use ($router) {
